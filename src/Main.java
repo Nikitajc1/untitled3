8cf52a1a36de4bd2a496fb2e51733c9ace314126
Nikita U <nikitajc@mail.ru>
2022-11-04 22:37:40 +0300

    Programm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 1000;
+        int y = 2000;
+        int z = x + y;
+        int w = y / 100;
+        int c = z + w;
+
+        if (y > 1000) {
+            System.out.println("Итоговый счёт: " + c);
+            System.out.println("Бонусных рублей: " + w);
+        }
+        else {
+            System.out.println("Итоговый счёт: " + z);
+            System.out.println("Бонусных рублей: 0");
+        }
     }
 }
